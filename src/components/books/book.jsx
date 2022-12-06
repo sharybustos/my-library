697979a8f4b97b8cd673326604855c720a141e48
sharybustos <scbustos@ubits.co>
2022-12-06 02:21:23 -0500

    Added book view

--- src/components/books/book.jsx
+++ src/components/books/book.jsx
@@ -1,21 +1,40 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import dateFormat from "dateformat"
 
 const Book = () => {
     const { id } = useParams()
+    const [book, setBook] = useState(null)
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:4000/books/" + id)
+            .then(response => {
+                console.log(response.data)
+                const readAt = response.data.readAt
+                response.data['readAt'] = dateFormat(readAt, "yyyy-mm-dd")
+                setBook(response.data)
+            })
+    }, [])
 
     return (
-        <div>
-            <h2>#{id} Beloved</h2>
-            <h4>Toni Morrison</h4>
-            <p>Lorem ipsum dolor sit
-                amet consectetur adipisicing
-                elit. Dolore excepturi quos
-                alias cumque sit asperiores commodi
-                nemo iure cupiditate aspernatur eaque
-                consectetur libero, assumenda, saepe
-                delectus. Dolorem est vel placeat.
-            </p>
-        </div>
+        <>
+            {book && (<div>
+                <h2>Name: {book.title}</h2>
+                <h4>Author: {book.author}</h4>
+                <h4>Year: {book.year}</h4>
+                <p>Lorem ipsum dolor sit
+                    amet consectetur adipisicing
+                    elit. Dolore excepturi quos
+                    alias cumque sit asperiores commodi
+                    nemo iure cupiditate aspernatur eaque
+                    consectetur libero, assumenda, saepe
+                    delectus. Dolorem est vel placeat.
+                </p>
+                <h4>Read at: {book.readAt}</h4>
+            </div>)}
+        </>
     )
 }
 
