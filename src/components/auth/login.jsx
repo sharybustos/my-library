bff9df0dc9583e20f5b144cee4b7efd4cf73a376
sharybustos <scbustos@ubits.co>
2022-12-06 02:33:16 -0500

    Added login form and API consumption

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,47 +1,48 @@
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm();
-    const customSubmit = (data) => { console.log('login data', data) }
+    const navigate = useNavigate();
+    const { register, formState: { errors }, handleSubmit, setValue } = useForm();
+    const customSubmit = (data) => {
+        axios
+            .post("http://localhost:4000/users/login", data)
+            .then(response => {
+                if (response.data.length != 0) {
+                    localStorage.setItem('user_id', response.data[0]._id)
+                    navigate("/books")
+                } else {
+                    alert('Invalid input data. Please, try again')
+                    setValue('password', '')
+                }
+            })
+    }
 
     return (
-        <form className="form" onSubmit={handleSubmit(customSubmit)}>
-            <div className="form__item">
-                <label>FirstName</label>
-                <input
-                    {...register("firstName", { required: true, maxLength: 10 })}
-                    aria-invalid={errors.firstName ? "true" : "false"}
-                />
-                {errors.firstName && <p>Field required. Max length 10</p>}
-            </div>
-            <div className="form__item">
-                <label>LastName</label>
-                <input
-                    {...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i })}
-                    aria-invalid={errors.lastName ? "true" : "false"}
-                />
-                {errors.lastName && <p>Field required. ABC required</p>}
-            </div>
-            <div className="form__item">
-                <label>Age</label>
-                <input
-                    type="number"
-                    {...register("age", { min: 18, max: 99 })}
-                    aria-invalid={errors.age ? "true" : "false"}
-                />
-                {errors.age && <p>Min 18. Max 99</p>}
-            </div>
-            <div className="form__item">
-                <label>Date</label>
-                <input
-                    type="date"
-                    {...register("date", { required: true })}
-                    aria-invalid={errors.date ? "true" : "false"}
-                />
-                {errors.date && <p>Field required</p>}
-            </div>
-            <input type="submit" value="Send" />
-        </form>
+        <>
+            <h2>Welcome to Login</h2>
+            <form className="form" onSubmit={handleSubmit(customSubmit)}>
+                <div className="form__item">
+                    <label>Username</label>
+                    <input
+                        {...register("username", { required: true, maxLength: 10 })}
+                        aria-invalid={errors.userName ? "true" : "false"}
+                    />
+                    {errors.userName && <p>Field required. Max length 10</p>}
+                </div>
+                <div className="form__item">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        {...register("password", { required: true, minLength: 6 })}
+                        aria-invalid={errors.password ? "true" : "false"}
+                    />
+                    {errors.password && <p>Field required. Min length 6</p>}
+                </div>
+                <input type="submit" value="Send" />
+            </form>
+        </>
     )
 }
 
