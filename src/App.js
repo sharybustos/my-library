cfdf4d9d2978bd6dfaa6aea77d8e5dc9b4885b02
sharybustos <scbustos@ubits.co>
2022-12-06 02:51:12 -0500

    Added guards to routes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,58 @@ import BooksHome from './components/books/books-home';
 import Book from './components/books/book';
 import BookEdit from './components/books/book-edit';
 import BookNew from './components/books/book-new';
-import Contenedor from './components/contenedor';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from './components/auth/register';
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from './components/navbar';
+import { useState } from 'react';
+import Guard from './components/auth/guard';
+import { useEffect } from 'react';
 
 function App() {
+  const [user, setUser] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (localStorage.getItem('user_id')) {
+      setUser(true)
+    } else {
+      setUser(false)
+    }
+  }, [location])
+
   return (
 
-    <BrowserRouter>
-      <Navbar />
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/books" element={<BooksHome />} />
-          <Route path="/books/:id" element={<Book />} />
-          <Route path="/books/:id/edit" element={<BookEdit />} />
-          <Route path="/books/new" element={<BookNew />} />
-          <Route path="/contenedor" element={<Contenedor />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar userProps={user} />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/books"
+          element={
+            <Guard isAllowed={user}>
+              <BooksHome />
+            </Guard>} />
+        <Route
+          path="/books/:id"
+          element={
+            <Guard isAllowed={user}>
+              <Book />
+            </Guard>} />
+        <Route
+          path="/books/:id/edit"
+          element={
+            <Guard isAllowed={user}>
+              <BookEdit />
+            </Guard>} />
+        <Route
+          path="/books/new"
+          element={
+            <Guard isAllowed={user}>
+              <BookNew />
+            </Guard>} />
+      </Routes>
+    </div>
   )
 }
 
