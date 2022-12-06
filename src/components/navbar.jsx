5de9770247d7ece170fabd28380717bbfd16ceb0
sharybustos <scbustos@ubits.co>
2022-12-06 02:59:26 -0500

    Added navbar validations

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,50 @@
 import { NavLink } from "react-router-dom"
+import { useEffect, useState } from "react"
 
-const Navbar = () => {
+const Navbar = ({ userProps }) => {
+    const [user, setUser] = useState(false)
+
+    useEffect(() => {
+        console.log(userProps)
+        setUser(userProps)
+    })
+
+    const logOut = () => {
+        localStorage.removeItem('user_id')
+        setUser(false)
+    }
+
     return (
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">My Library</a>
                 <div id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        {user ? <li className="nav-item">
                             <NavLink
                                 style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
                                 className="nav-link"
                                 to="/books">Books</NavLink>
-                        </li>
-                        <li className="nav-item">
+                        </li> : ''}
+                        {user ? '' : <li className="nav-item">
                             <NavLink
                                 style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
                                 className="nav-link"
                                 to="/login">Login</NavLink>
-                        </li>
+                        </li>}
+                        {user ? '' : <li className="nav-item">
+                            <NavLink
+                                style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
+                                className="nav-link"
+                                to="/register">Register</NavLink>
+                        </li>}
+                        {user ? <li className="nav-item">
+                            <NavLink
+                                style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
+                                className="nav-link"
+                                to="/login"
+                                onClick={() => logOut()}>Log out</NavLink>
+                        </li> : ''}
                     </ul>
                 </div>
             </div>
